ca49bafeb038ee6eccf417a5542ee092e81e5f29
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2025-01-28 21:28:13 +0100

    inin2i3

--- src/components/RoomCanvas.tsx
+++ src/components/RoomCanvas.tsx
@@ -5,7 +5,7 @@ import { Room } from './Room';
 
 export function RoomCanvas() {
   const currentRoom = useStore((state) => state.currentRoom);
-
+  console.log(currentRoom);
   return (
     <div className="w-full h-[600px] rounded-lg overflow-hidden bg-gray-900">
       <Canvas camera={{ position: [0, 2, 5], fov: 75 }}>
